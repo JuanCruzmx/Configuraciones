2644b6acd8f904753adf49c38a26e1434a584ece
JuanCruz <juancruzosorio.mx@gmail.com>
2026-04-25 01:42:43 -0600

    Configuración Vim

--- .vimrc
+++ .vimrc
@@ -14,5 +14,21 @@ set clipboard=unnamedplus
 "  --- Estilos ---
 set background=dark
 colorscheme desert
-"  --- Comandos ---
-nnoremap <C-z> :w<CR>:!pdflatex %<CR>
+"  --- Comandos y funciones ---
+autocmd BufEnter *.tex nnoremap <buffer> <C-b> :w<CR>:!pdflatex % && rm -f "%:r.aux" "%:r.log" "%:r.out"<CR>:!explorer.exe "%:r.pdf"<CR><CR>
+function! PlantillaLatex()
+    call setline(1, '\documentclass{article}')
+    call setline(2, '\usepackage[utf8]{inputenc}')
+    call setline(3, '\usepackage[spanish]{babel}')
+    call setline(4, '\usepackage{amsmath, amssymb}')
+    call setline(5, '')
+    call setline(6, '\title{}')
+    call setline(7, '\date{\today}')
+    call setline(8, '\author{Juan Cruz Osorio}')
+    call setline(9, '')
+    call setline(10, '\begin{document}')
+    call setline(11, '')
+    call setline(12, '\end{document}')
+   execute "normal! 11G" | startinsert
+endfunction
+autocmd BufNewFile *.tex call PlantillaLatex()
--- config-vim.py
+++ config-vim.py
@@ -6,15 +6,35 @@ def config_vim():
     sistema = platform.system()         # Detección del sistema
     home = os.path.expanduser("~")      # Busca carpeta de usuario
     
-    if sistema == "Windows":
-        nombre_archivo = "_vimrc"
+    if sistema == 'Windows':
+        nombre_vim = '_vimrc'
+        abrir_pdf = '!start chrome "%:r.pdf"'
+    elif sistema == 'Darwin':
+        nombre_vim = '.vimrc'
+        abrir_pdf = '!open -a "Google Chrome" "%:r.pdf"'
     else:
-        nombre_archivo = ".vimrc"
+        nombre_vim = ".vimrc"
+        abrir_pdf = '!explorer.exe "%:r.pdf"'
     
-    ruta = os.path.join(home, nombre_archivo)
+    ruta = os.path.join(home, nombre_vim)
     latex = shutil.which("pdflatex") is not None   
 
-    config = [
+    plantilla_latex = [
+        r'\documentclass{article}',
+        r'\usepackage[utf8]{inputenc}',
+        r'\usepackage[spanish]{babel}',
+        r'\usepackage{amsmath, amssymb}',
+        r'',
+        r'\title{}',
+        r'\date{\today}',
+        r'\author{Juan Cruz Osorio}',
+        r'',
+        r'\begin{document}',
+        r'',
+        r'\end{document}',
+    ]
+
+    vim = [
         '"  --- Configuraciones básicas ---',
         'syntax on',
         'set number',
@@ -31,12 +51,17 @@ def config_vim():
         '"  --- Estilos ---',
         'set background=dark',
 	    'colorscheme desert',
-        '"  --- Comandos ---',
-        'nnoremap <C-z> :w<CR>:!pdflatex %<CR>',
+        '"  --- Comandos y funciones ---',
+        f'autocmd BufEnter *.tex nnoremap <buffer> <C-b> :w<CR>:!pdflatex % && rm -f "%:r.aux" "%:r.log" "%:r.out"<CR>:{abrir_pdf}<CR><CR>',
+        'function! PlantillaLatex()',
+        *[f"    call setline({i+1}, '{linea.replace(chr(39),chr(39)+chr(39))}')" for i, linea in enumerate(plantilla_latex)],
+        '   execute "normal! 11G" | startinsert',
+        'endfunction',
+        'autocmd BufNewFile *.tex call PlantillaLatex()',
     ]
     
-    with open(ruta, 'w') as f:
-        f.write("\n".join(config))
+    with open(ruta, 'w', encoding='utf-8') as f:
+        f.write("\n".join(vim))
     
     print(f'¡Vim configurado en {sistema}!')
     print(f'Ruta: {ruta}')
