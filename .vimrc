ec6b10996df73d684d822a149a3531fd0d959e00
JuanCruz <juancruzosorio.mx@gmail.com>
2026-04-25 23:52:35 -0600

    Configuaciones de trabajo

--- .vimrc
+++ .vimrc
@@ -1,4 +1,4 @@
-"  --- Configuraciones básicas ---
+"  --- Configuraciones ---
 syntax on
 set number
 set relativenumber
@@ -14,21 +14,25 @@ set clipboard=unnamedplus
 "  --- Estilos ---
 set background=dark
 colorscheme desert
-"  --- Comandos y funciones ---
+"  --- Comandos ---
 autocmd BufEnter *.tex nnoremap <buffer> <C-b> :w<CR>:!pdflatex % && rm -f "%:r.aux" "%:r.log" "%:r.out"<CR>:!explorer.exe "%:r.pdf"<CR><CR>
+"  --- Funciones ---
 function! PlantillaLatex()
     call setline(1, '\documentclass{article}')
     call setline(2, '\usepackage[utf8]{inputenc}')
     call setline(3, '\usepackage[spanish]{babel}')
     call setline(4, '\usepackage{amsmath, amssymb}')
-    call setline(5, '')
-    call setline(6, '\title{}')
-    call setline(7, '\date{\today}')
-    call setline(8, '\author{Juan Cruz Osorio}')
-    call setline(9, '')
-    call setline(10, '\begin{document}')
-    call setline(11, '')
-    call setline(12, '\end{document}')
-   execute "normal! 11G" | startinsert
+    call setline(5, '\usepackage{graphicx}')
+    call setline(6, '\usepackage[margin=2.5cm]{geometry}')
+    call setline(7, '\usepackage{hyperref}')
+    call setline(8, '')
+    call setline(9, '\title{}')
+    call setline(10, '\date{\today}')
+    call setline(11, '\author{Juan Cruz Osorio}')
+    call setline(12, '')
+    call setline(13, '\begin{document}')
+    call setline(14, '')
+    call setline(15, '\end{document}')
+   execute "normal! 14G" | startinsert
 endfunction
 autocmd BufNewFile *.tex call PlantillaLatex()
--- config-vim.py
+++ config-vim.py
@@ -2,69 +2,81 @@ import os
 import platform
 import shutil
 
-def config_vim():
-    sistema = platform.system()         # Detección del sistema
-    home = os.path.expanduser("~")      # Busca carpeta de usuario
-    
-    if sistema == 'Windows':
-        nombre_vim = '_vimrc'
-        abrir_pdf = '!start chrome "%:r.pdf"'
-    elif sistema == 'Darwin':
-        nombre_vim = '.vimrc'
-        abrir_pdf = '!open -a "Google Chrome" "%:r.pdf"'
-    else:
-        nombre_vim = ".vimrc"
-        abrir_pdf = '!explorer.exe "%:r.pdf"'
-    
-    ruta = os.path.join(home, nombre_vim)
-    latex = shutil.which("pdflatex") is not None   
-
-    plantilla_latex = [
-        r'\documentclass{article}',
-        r'\usepackage[utf8]{inputenc}',
-        r'\usepackage[spanish]{babel}',
-        r'\usepackage{amsmath, amssymb}',
-        r'',
-        r'\title{}',
-        r'\date{\today}',
-        r'\author{Juan Cruz Osorio}',
-        r'',
-        r'\begin{document}',
-        r'',
-        r'\end{document}',
-    ]
-
-    vim = [
-        '"  --- Configuraciones básicas ---',
-        'syntax on',
-        'set number',
-        'set relativenumber',
-        'set mouse=a',
-	    'set tabstop=4',
-	    'set shiftwidth=4',
-	    'set expandtab',
-        'set cursorline',
-        'set laststatus=2',
-        'set showmode',
-        'set showmatch',
-        'set clipboard=unnamedplus',
-        '"  --- Estilos ---',
-        'set background=dark',
-	    'colorscheme desert',
-        '"  --- Comandos y funciones ---',
-        f'autocmd BufEnter *.tex nnoremap <buffer> <C-b> :w<CR>:!pdflatex % && rm -f "%:r.aux" "%:r.log" "%:r.out"<CR>:{abrir_pdf}<CR><CR>',
-        'function! PlantillaLatex()',
-        *[f"    call setline({i+1}, '{linea.replace(chr(39),chr(39)+chr(39))}')" for i, linea in enumerate(plantilla_latex)],
-        '   execute "normal! 11G" | startinsert',
-        'endfunction',
-        'autocmd BufNewFile *.tex call PlantillaLatex()',
-    ]
-    
-    with open(ruta, 'w', encoding='utf-8') as f:
-        f.write("\n".join(vim))
-    
-    print(f'¡Vim configurado en {sistema}!')
-    print(f'Ruta: {ruta}')
+class Config:
+    def __init__(self):
+        self.os = platform.system()         # Detección del sistema
+        self.home = os.path.expanduser("~")      # Busca carpeta de usuario
+        self.herramientas = ['vim', 'git', 'pdflatex', 'python3', 'gcc']    
+        self.sistema()
+        self.verificar()
+    def sistema(self): 
+        if self.os == 'Windows':
+            self.nombre_vim = '_vimrc'
+            self.abrir_pdf = '!start chrome "%:r.pdf"'
+        elif self.os == 'Darwin':
+            self.nombre_vim = '.vimrc'
+            self.abrir_pdf = '!open -a "Google Chrome" "%:r.pdf"'
+        else:
+            self.nombre_vim = ".vimrc"
+            self.abrir_pdf = '!explorer.exe "%:r.pdf"'
+    def plantilla_latex(self):
+        self.plantilla = [
+            r'\documentclass{article}',
+            r'\usepackage[utf8]{inputenc}',
+            r'\usepackage[spanish]{babel}',
+            r'\usepackage{amsmath, amssymb}',
+            r'\usepackage{graphicx}',
+            r'\usepackage[margin=2.5cm]{geometry}',
+            r'\usepackage{hyperref}',
+            r'',
+            r'\title{}',
+            r'\date{\today}',
+            r'\author{Juan Cruz Osorio}',
+            r'',
+            r'\begin{document}',
+            r'',
+            r'\end{document}',
+        ]
+    def config_vim(self):
+        self.vim  = [
+            '"  --- Configuraciones ---',
+            'syntax on',
+            'set number',
+            'set relativenumber',
+            'set mouse=a',
+            'set tabstop=4',
+            'set shiftwidth=4',
+            'set expandtab',
+            'set cursorline',
+            'set laststatus=2',
+            'set showmode',
+            'set showmatch',
+            'set clipboard=unnamedplus',
+            '"  --- Estilos ---',
+            'set background=dark',
+            'colorscheme desert',
+            '"  --- Comandos ---',
+            f'autocmd BufEnter *.tex nnoremap <buffer> <C-b> :w<CR>:!pdflatex % && rm -f "%:r.aux" "%:r.log" "%:r.out"<CR>:{self.abrir_pdf}<CR><CR>',
+            '"  --- Funciones ---',
+            'function! PlantillaLatex()',
+            *[f"    call setline({i+1}, '{linea.replace(chr(39),chr(39)+chr(39))}')" for i, linea in enumerate(self.plantilla)],
+            '   execute "normal! 14G" | startinsert',
+            'endfunction',
+            'autocmd BufNewFile *.tex call PlantillaLatex()',
+        ]
+    def archivos(self):
+        self.plantilla_latex()
+        self.config_vim()
+        self.ruta = os.path.join(self.home, self.nombre_vim)
+        with open(self.ruta, 'w', encoding='utf-8') as f:
+            f.write("\n".join(self.vim))
+    def verificar(self): 
+        self.archivos()
+        print(f'Sistema Operativo: {self.os}')
+        print(f'Vimrc: {self.ruta}')
+        for h in self.herramientas:
+            verificar = shutil.which(h) is not None
+            print(f'{h.ljust(10)} | {str(shutil.which(h) is not None).ljust(10)} | {shutil.which(h)}')
 
 if __name__ == '__main__':
-    config_vim()
+    config = Config()
