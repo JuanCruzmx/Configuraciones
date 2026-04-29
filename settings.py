c9b1e76f4dc48f942fd09512e4ee47d37583e6f9
JuanCruz <juancruzosorio.mx@gmail.com>
2026-04-29 14:24:30 -0600

    Settings

--- settings.py
+++ settings.py
@@ -1,6 +1,48 @@
 import platform
+import os
 
 class Settings:
     def __init__(self):
         self.os = platform.system()
-        if 
+        home = os.path.expanduser('~')
+
+        if self.os == 'Windows':
+            self.ruta = os.path.join(home, '_vimrc')
+        else:
+            self.ruta = os.path.join(home, '.vimrc')
+
+    def vim(self):
+        return configuration = [
+               'set number',
+               'set cursorline',
+               'set mouse=a',
+               'set expandtab',
+               'set tabstop=4',
+               'set shiftwidth=4',
+               'set autoindent',
+               'set noswapfile',
+               'set nobackup',
+               '"   --- Comandos ---',
+               'nnoremap <le
+               ]
+
+    def latex(self):
+        return template = [
+                r'\documentclass{article}',
+                r'\usepackage[spanish]{babel}',
+                r'\usepackage[margin=2.5cm]{geometry}',
+                r'\usepackage{amsmath, amssymb}',
+                r'\usepackage{graphicx}',
+                r'\usepackage{hyperref}',
+                r'\begin{document}',
+                r'',
+                r'\end{document}'
+                ]
+
+    def show(self):
+        print(f'Sistema: {self.os}')
+        print(f'Vimrc: {self.ruta}')
+
+if __name__ == '__main__':
+    settings = Settings()
+    settings.show()
