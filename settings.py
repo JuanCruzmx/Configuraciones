d13542211d8414ae600e3fa0aac7a5d81097c2e5
JuanCruz <juancruzosorio.mx@gmail.com>
2026-04-30 14:09:15 -0600

    Settings

--- settings.py
+++ settings.py
@@ -12,34 +12,44 @@ class Settings:
             self.ruta = os.path.join(home, '.vimrc')
 
     def vim(self):
-        return configuration = [
-               'set number',
-               'set cursorline',
-               'set mouse=a',
-               'set expandtab',
-               'set tabstop=4',
-               'set shiftwidth=4',
-               'set autoindent',
-               'set noswapfile',
-               'set nobackup',
-               '"   --- Comandos ---',
-               'nnoremap <le
-               ]
+        return [
+            'syntax on',
+            'set t_Co=256',
+            'set background=dark',
+            '',
+            'set number',
+            'set relativenumber', 
+            'set cursorline',
+            '',
+            'set mouse=a',
+            'set expandtab',
+            'set tabstop=4',
+            'set shiftwidth=4',
+            '',
+            'set autoindent',
+            'set noswapfile',
+            'set nobackup',
+            '"   --- Comandos ---',
+            '"nnoremap <leader>w :w<CR>'
+        ]
 
     def latex(self):
-        return template = [
-                r'\documentclass{article}',
-                r'\usepackage[spanish]{babel}',
-                r'\usepackage[margin=2.5cm]{geometry}',
-                r'\usepackage{amsmath, amssymb}',
-                r'\usepackage{graphicx}',
-                r'\usepackage{hyperref}',
-                r'\begin{document}',
-                r'',
-                r'\end{document}'
-                ]
+        return [
+            r'\documentclass{article}',
+            r'\usepackage[spanish]{babel}',
+            r'\usepackage[margin=2.5cm]{geometry}',
+            r'\usepackage{amsmath, amssymb}',
+            r'\usepackage{graphicx}',
+            r'\usepackage{hyperref}',
+            r'\begin{document}',
+            r'',
+            r'\end{document}'
+        ]
 
     def show(self):
+        vim = self.vim()
+        with open(self.ruta, 'w') as files:
+            files.write('\n'.join(vim))
         print(f'Sistema: {self.os}')
         print(f'Vimrc: {self.ruta}')
 
