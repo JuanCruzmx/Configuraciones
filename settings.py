99ae765d08f0cec0ced3fa44e54830218a8c7055
JuanCruz <juancruzosorio.mx@gmail.com>
2026-07-30 23:04:39 -0600

    Configuraciones (nueva version) Parte 1

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-import os, platform, shutil
+import os, platform, shutil, subprocess, sys, re
 
 sistem = platform.system()
 home = os.path.expanduser("~") 
@@ -14,14 +14,32 @@ else:
     delete = "rm -f"
 
 def software():
-    software = ["vim", "git", "gcc", "pdflatex", "python3"]
-    print(f"{'software':<15} {'version'}")
+    software = ["vim", "git", "gcc", "pdflatex", "python3", "java"]
+    print(f"{'software':<15}{'version'}")
     print(f"{'-'*30}")
 
     for i in software:
-        version = shutil.which(i)
-        print(f"{i:<15} {version}")
+        verificar = shutil.which(i)
+        version = subprocess.run([i, "--version"], capture_output=True, text=True, check=True)
+        if not verificar:
+            print(f"{i}")
+            continue
+        else:
+            print(f"{i:<15}{re.search(r'\d+(\.\d+)+', version.stdout.splitlines()[0]).group(0)}")
 
 
+intentos = 1
 print(f"Sistema: {sistem}")
-software()
+while True:
+    opcion = input("Ver software instalados [Y/N]: ").upper()
+    if opcion == "Y":
+        software()
+        break
+    elif opcion == "N":
+        break 
+    else:
+        if intentos == 3:
+            sys.exit()
+        intentos += 1
+
+print("Continua")
